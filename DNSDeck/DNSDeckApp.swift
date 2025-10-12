15a3dd0d6e9b8d66e99adaccc3efc4457bab1385
Praveen Thirumurugan <praveen.thirumurugan@postman.com>
2025-10-12 17:47:19 +0530

    Initial Commit

---
+++ DNSDeck/DNSDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNSDeckApp.swift
+//  DNSDeck
+//
+//  Created by Praveen Thirumurugan on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DNSDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
